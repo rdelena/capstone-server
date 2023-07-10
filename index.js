2cf26509f83dd73e020e9a20d8ab4cb92bfdd9f7
rdelena <rodnydelena@gmail.com>
2023-07-09 19:27:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRouter = require("./routers/users");
+const app = express();
+const port = process.env.PORT || 4001;
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}`);
+});
---
+++ sql/connection.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log("creating connection...");
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PW,
+        database: process.env.DB,
+      });
+
+      return this.pool;
+    }
+
+    return this.pool;
+  }
+}
+
+const instance = new Connection();
+module.exports = instance;
