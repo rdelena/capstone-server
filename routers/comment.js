d183b0407f8536bf551de920d490008eb2c80760
rdelena <rodnydelena@gmail.com>
2023-08-20 01:24:25 -0500

    test remove jwt

--- routers/comment.js
+++ routers/comment.js
@@ -3,7 +3,7 @@ const commentController = require("../controllers/comment");
 const router = express.Router();
 const { verifyJwt } = require("../middleware/auth");
 
-router.get("/", verifyJwt, commentController.getAllComments);
+router.get("/", commentController.getAllComments);
 router.post("/", verifyJwt, commentController.postComment);
 
 module.exports = router;
